0340cf12bd3bcd31e843d24ed8eee80cad4d6cfb
Almog <almoglemberg@gmail.com>
2022-01-13 17:15:03 +0200

    Updates

---
+++ _nuxt/d6ec5c8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{253:function(e,n,t){"use strict";t.r(n);var c={name:"IndexPage"},l=t(56),component=Object(l.a)(c,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("production test")])}),[],!1,null,null,null);n.default=component.exports}}]);
